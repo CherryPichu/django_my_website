62fb1d1af8cb844a23546e714643ca2e79b7382e
CherryPichu <uskawjdu@gmail.com>
2021-01-02 20:59:12 +0900

    time : Seoul; Post: str

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     creaded = models.DateField() #언제 작성이 되는지
 
     author = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL) # 어떤 사용자가 사용이 되는지 유저라는 객체를 연결시켜줌
+    def __str__(self): # 오버라이딩 이 객체를 문자를 바꾸었을 때 어떻게 보여줄 것인지
+        return '{}::{}'.format(self.title, self.author) # 제목 abc
     # 누가 들어올 수 있다.
     #on_delate = True 사용자가 탈퇴를 할 때 글도 삭제할꺼냐?
 
--- my_site_prj/settings.py
+++ my_site_prj/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul' # 시간 설정하기
 
 USE_I18N = True
 
